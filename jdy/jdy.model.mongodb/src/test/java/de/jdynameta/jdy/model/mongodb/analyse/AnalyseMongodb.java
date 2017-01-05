cfe10fa0997524c79f880157d4a7f8df5c08cf14
Rainer Schneider <rainer.schneider@exxcellent.de>
2017-01-05 06:29:08 +0100

    de.flapdoodle.embed.mongo

--- jdy/jdy.model.mongodb/src/test/java/de/jdynameta/jdy/model/mongodb/analyse/AnalyseMongodb.java
+++ jdy/jdy.model.mongodb/src/test/java/de/jdynameta/jdy/model/mongodb/analyse/AnalyseMongodb.java
@@ -16,7 +16,21 @@
 package de.jdynameta.jdy.model.mongodb.analyse;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongoCmdOptionsBuilder;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.config.Storage;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.io.file.Files;
+import de.flapdoodle.embed.process.runtime.Network;
+import java.io.File;
+import java.io.IOException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -30,37 +44,81 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import static java.util.Arrays.asList;
+
 /**
  *
  * @author rschneider
  */
 public class AnalyseMongodb {
-    
+
+    private static final int PORT = 12345;
+    private static MongodExecutable _mongodExe;
+    private static MongodProcess _mongod;
+
     public AnalyseMongodb() {
     }
-    
+
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws IOException {
+
+        final String DB_FOLDER_NAME = "c:/tmp/testdbdir";
+        
+        try {
+            Files.forceDelete(new File(DB_FOLDER_NAME));
+        } catch (Exception e) {
+
+        }
+
+        new File(DB_FOLDER_NAME).mkdirs();
+
+        IMongodConfig mongodConfig = new MongodConfigBuilder()
+                .version(Version.Main.PRODUCTION)
+                .configServer(false)
+                .replication(new Storage(DB_FOLDER_NAME, null, 0))
+                .net(new Net(PORT, Network.localhostIsIPv6()))
+                .cmdOptions(new MongoCmdOptionsBuilder()
+                        .syncDelay(0)
+                        .useNoPrealloc(true)
+                        .useSmallFiles(true)
+                        .useNoJournal(true)
+                        .build())
+                .build();           
+           MongodStarter starter = MongodStarter.getDefaultInstance();
+
+        _mongodExe = starter.prepare(mongodConfig);
+        _mongod = _mongodExe.start();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
+        _mongod.stop();
+        _mongodExe.stop();
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
     public void hello() throws ParseException {
+
+
+        try (MongoClient mongoClient = new MongoClient("localhost", PORT)) {
+            MongoDatabase db = mongoClient.getDatabase("jdy");
+            insertData(db);
+            showRestaurants(mongoClient);
+        }
+    }
+
+    private void showRestaurants(MongoClient mongoClient) {
         
-        MongoClient mongoClient = new MongoClient();
-        MongoDatabase db = mongoClient.getDatabase("jdy");
-        insertData(db);
+        MongoDatabase database = mongoClient.getDatabase("jdy");
+        MongoCollection<Document> restaurants =  database.getCollection("restaurants");
+        System.out.println("Restaurants " + restaurants.count());
     }
     
     private void insertData(MongoDatabase db) throws ParseException {
@@ -69,23 +127,23 @@ public class AnalyseMongodb {
         db.getCollection("restaurants").insertOne(
                 new Document("address",
                         new Document()
-                                .append("street", "2 Avenue")
-                                .append("zipcode", "10075")
-                                .append("building", "1480")
-                                .append("coord", asList(-73.9557413, 40.7720266)))
-                        .append("borough", "Manhattan")
-                        .append("cuisine", "Italian")
-                        .append("grades", asList(
-                                new Document()
-                                        .append("date", format.parse("2014-10-01T00:00:00Z"))
-                                        .append("grade", "A")
-                                        .append("score", 11),
-                                new Document()
-                                        .append("date", format.parse("2014-01-16T00:00:00Z"))
-                                        .append("grade", "B")
-                                        .append("score", 17)))
-                        .append("name", "Vella")
-                        .append("restaurant_id", "41704620"));        
+                        .append("street", "2 Avenue")
+                        .append("zipcode", "10075")
+                        .append("building", "1480")
+                        .append("coord", asList(-73.9557413, 40.7720266)))
+                .append("borough", "Manhattan")
+                .append("cuisine", "Italian")
+                .append("grades", asList(
+                        new Document()
+                        .append("date", format.parse("2014-10-01T00:00:00Z"))
+                        .append("grade", "A")
+                        .append("score", 11),
+                        new Document()
+                        .append("date", format.parse("2014-01-16T00:00:00Z"))
+                        .append("grade", "B")
+                        .append("score", 17)))
+                .append("name", "Vella")
+                .append("restaurant_id", "41704620"));
     }
-    
+
 }
